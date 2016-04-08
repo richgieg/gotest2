78e3aa3a06d5c0037e9e7cf95f899f4ea1b67dbc
Richard Gieg <richgieg@users.noreply.github.com>
2016-04-07 23:19:49 -0700

    Add Person type

--- main.go
+++ main.go
@@ -4,4 +4,7 @@ import "fmt"
 
 func main() {
 	fmt.Println("gotest2")
+
+	p := NewPerson("John", 45)
+	p.Talk()
 }
---
+++ person.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func NewPerson(name string, age int) *Person {
+	p := new(Person)
+	p.Name = name
+	p.Age = age
+	return p
+}
+
+func (p *Person) Talk() {
+	fmt.Printf("Hi! My name is %s\n", p.Name)
+}
